7a8e26d807458d63925b17f4fa0d083fe785246e
mitinhobr <141642157+isabellaberwanger@users.noreply.github.com>
2024-10-10 07:58:43 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,30 @@
-
+criaCartao{
+    'filme',
+    'qual é a diferença da Bruxa Má do Oeste do livro para o filme?',
+    'A Bruxa Má do Oeste do filme O Mágico de Oz tem dois olhos, enquanto no livro tem apenas um.',
+}
+criaCartao{
+    'filme',
+    'quantos oscars ganhou O Mágico de oz?',
+    'Em 1940, O Mágico de Oz venceu o Oscar de melhor trilha sonora original e de melhor música.',
+}
+criaCartao{
+    'jogo',
+    'Como era a criação de Kratos durante a produção? ',
+    'Além de ter um cabelo muito longo, o protagonista carregava um bebê.',
+}
+criaCartao{
+    'jogo',
+    'Qual é a altura de Kratos?',
+    'o personagem tem 1,98 m de altura.'
+}
+criaCartao{
+    'categoria',
+    'pergunta',
+    'resposta',
+}
+criaCartao{
+    'categoria',
+    'pergunta',
+    'resposta',
+}
